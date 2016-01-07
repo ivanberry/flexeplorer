61294d0a74c0372297fb10b964dca796f84e2446
ivanberry <ivansync@outlook.com>
2016-01-07 20:58:49 +0800

    finish the all makeup and css

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,20 @@ var gulp = require('gulp');
 var less = require('gulp-less'),
 	//autoprefixer = require('gulp-autoprefixer'),
 	//cssmin = require('gulp-cssmin')
-	browserSync = require('browser-sync').create();
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass');
+
+
+//JS插件
+var jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish');
+
+gulp.task('lint', function() {
+	return gulp.src('./js/custom.js') //针对自己写的脚本hint
+	    .pipe(jshint())
+		.pipe(jshint.reporter(stylish));
+
+		});
 
 	//Static server and watching the less/html files
 
@@ -20,6 +33,7 @@ gulp.task('server', ['less'], function() {
 	gulp.watch("./less/**/*.less", ['less', 'less-watch']);
 	gulp.watch("./*.html").on('change', browserSync.reload);
 	gulp.watch("./css/*.css").on('change', browserSync.reload);
+	gulp.watch("./hs/*.js").on('change', browserSync.reload);
 
 });
 
@@ -32,7 +46,15 @@ gulp.task('less', function(){
 
 });
 
+gulp.task('sass', function(){
+	return gulp.src("sass/main.sass")
+	       .pipe(sass())
+	       .pipe(gulp.dest("./css"))
+	       .pipe(browserSync.stream());
+	
+});
+
 gulp.task('less-watch', ['less'], browserSync.reload);
-gulp.task('default', ['server']);
+gulp.task('default', ['server', 'lint']);
 
 
